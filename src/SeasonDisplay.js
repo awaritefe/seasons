7b182a836f3e50d7b395e82d7a8bcf5108390faa
DanielAwaritefe <awaritefe@gmail.com>
2021-04-23 19:10:05 +0100

    added css styling to seasons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,4 +1,17 @@
+import "./SeasonDisplay.css"
 import React from "react";
+
+
+const seasonConfig = {
+    summer: {
+        promt: "Let's go to the Beach!",
+        iconName: "sun"
+    },
+    winter: {
+        promt: "Burr its chilly!",
+        iconName: "snowflake"
+    }
+};
 
 const getSeason = (lat, month) => {
     if (month > 2 && month < 9) {
@@ -8,16 +21,19 @@ const getSeason = (lat, month) => {
     }
 }
 
-const SeasonDisplay = (props) => {
+const SeasonDisplay = props => {
    const season = getSeason(props.lat, new Date().getMonth());
-    const promt = season === "winter" ? "Burr its chilly!" : "Lets go to the Beach!";
-    const icon = season === "summer" ? "sun" : "snowflake";
+   const {promt, iconName} = seasonConfig[season];
+    // const promt = season === "winter" ? "Burr its chilly!" : "Lets go to the Beach!";
+    // const icon = season === "summer" ? "sun" : "snowflake";
 
-    return <div>
-        <i className={`${icon} icon`} />
-        <h1>{promt}</h1>
-        <i className={`${icon} icon`} />
-        </div>;
+    return (
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`} />
+            <h1>{promt}</h1>
+            <i className={`icon-right massive ${iconName} icon`} />
+        </div>
+    );
 };
 
 export default SeasonDisplay;
