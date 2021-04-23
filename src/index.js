43a456ee51047a66cd1d1f88e333d3af95ef40fa
DanielAwaritefe <awaritefe@gmail.com>
2021-04-23 20:16:24 +0100

    added loader and default message with default prop

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
-
+import Loading from "./Loading.js";
 if (module.hot) {
   module.hot.accept();
 }
@@ -33,7 +33,7 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />
     }
 
-    return <div>Loading!</div>;
+    return <Loading message="Please accept the location request" />;
   }
 }
 
